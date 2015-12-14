7de9f98871caad0aab8d0adc9aa25ea96b9aa62a
Ethan Kim <ethankim67@gmail.com>
2015-12-14 21:22:25 +0900

    update

---
+++ awsdemo/attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Author:: Piljoong Kim
+# Cookbook Name:: awsdemo
+#
+
+default['java']['java_home'] = '/usr/bin/jvm/java'
+
+default['java']['jdk_url'] = 'https://s3-us-west-2.amazonaws.com/awsdemo-test/jdk-8u65-linux-x64.gz'
+
--- awsdemo/recipes/setup.rb
+++ awsdemo/recipes/setup.rb
@@ -2,15 +2,48 @@
 
 #arch = node['jdk']['arch']
 #jdk_version = node['jdk']['version']
-build = 17
-arch = 'x64'
-jdk_version = '1.8.0_66'
-checksum = '7e95ad5fa1c75bc65d54aaac9e9986063d0a442f39a53f77909b044cef63dc0a'
-file_cache_path = nil
+#build = 17
+#arch = 'x64'
+#jdk_version = '1.8.0_66'
+#checksum = '7e95ad5fa1c75bc65d54aaac9e9986063d0a442f39a53f77909b044cef63dc0a'
+file_cache_path = 1
 
-jdk = "jdk-#{jdk_version}-linux-#{arch}.tar.gz"
+#jdk = "jdk-#{jdk_version}-linux-#{arch}.tar.gz"
 
-download_url = "http://download.oracle.com/otn-pub/java/jdk/#{jdk_version}-b#{build}/#{jdk}"
+#download_url = "http://download.oracle.com/otn-pub/java/jdk/#{jdk_version}-b#{build}/#{jdk}"
+
+java_home = node['java']['java_home']
+jdk_url = node['java']['jdk_url']
+
+ruby_block "set-env-java-home" do
+  block do
+    ENV["JAVA_HOME"] = java_home
+  end
+  not_if { ENV["JAVA_HOME"] == java_home }
+end
+
+file "/etc/profile.d/jdk.sh" do
+  content <<-EOS
+export JAVA_HOME=#{node['java']['java_home']}
+EOS
+  mode 0755
+end
+
+remote_file '/tmp/jdk.tar.gz' do
+  source jdk_url
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+
+#java_ark "jdk" do
+#  url jdk_url
+#  app_home java_home
+#  bin_alternatives_priority 1062
+#  action :install
+#end
 
 
 unless file_cache_path
@@ -23,7 +56,7 @@ unless file_cache_path
     compile_time false if Chef::Resource::ChefGem.method_defined?(:compile_time)
   end
 
-  Chef::Log.info("download #{download_url}")
+  Chef::Log.info("download #{download_url}"
   ruby_block "fetch #{download_url}" do
     block do
       JavaSE::Downloader.fetch(download_url, file_cache_path)
@@ -32,15 +65,15 @@ unless file_cache_path
   end
 end
 
-ruby_block "validate #{file_cache_path}" do
-  block do
-    JavaSE::Downloader.validate(file_cache_path, checksum)
-  end
-end
+#ruby_block "validate #{file_cache_path}" do
+#  block do
+#    JavaSE::Downloader.validate(file_cache_path, checksum)
+#  end
+#end
 
   
 
-package "tomcat7" do
-  action :install
-end
+#package "tomcat7" do
+#  action :install
+#end
 
