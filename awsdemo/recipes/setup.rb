143f9c2226967cdc5e2c6762e90a89ab6e5b3ff4
Ethan Kim <ethankim67@gmail.com>
2015-12-14 21:34:05 +0900

    update

--- awsdemo/recipes/setup.rb
+++ awsdemo/recipes/setup.rb
@@ -1,17 +1,4 @@
 
-
-#arch = node['jdk']['arch']
-#jdk_version = node['jdk']['version']
-#build = 17
-#arch = 'x64'
-#jdk_version = '1.8.0_66'
-#checksum = '7e95ad5fa1c75bc65d54aaac9e9986063d0a442f39a53f77909b044cef63dc0a'
-file_cache_path = 1
-
-#jdk = "jdk-#{jdk_version}-linux-#{arch}.tar.gz"
-
-#download_url = "http://download.oracle.com/otn-pub/java/jdk/#{jdk_version}-b#{build}/#{jdk}"
-
 java_home = node['java']['java_home']
 jdk_url = node['java']['jdk_url']
 
@@ -41,27 +28,8 @@ rpm_package 'jdk' do
   source '/tmp/jdk.rpm'
   action :install
 end
-
   
-
-
-#java_ark "jdk" do
-#  url jdk_url
-#  app_home java_home
-#  bin_alternatives_priority 1062
-#  action :install
-#end
-
-
-#ruby_block "validate #{file_cache_path}" do
-#  block do
-#    JavaSE::Downloader.validate(file_cache_path, checksum)
-#  end
-#end
-
-  
-
-#package "tomcat7" do
-#  action :install
-#end
+package "tomcat7" do
+  action :install
+end
 
